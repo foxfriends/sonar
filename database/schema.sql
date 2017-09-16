5487d16ae229997047a38f0ef3d35f650d939753
Cameron Eldridge <cameldridge+git@gmail.com>
2017-09-16 02:03:50 -0400

    placeholders for api requests

--- database/schema.sql
+++ database/schema.sql
@@ -10,26 +10,28 @@ CREATE TABLE users (
   password  VARCHAR(512) NOT NULL,
   join_date TIMESTAMP NOT NULL DEFAULT (NOW()::TIMESTAMP),
   avatar    VARCHAR(512),
-  score     INT,
+  score     INT DEFAULT 0,
   current_playing VARCHAR(512),
-  coord     POINT
+  coord     POINT -- (LNG, LAT) ! don't forget
+  -- NOTE: see https://www.postgresql.org/docs/9.1/static/earthdistance.html
 );
 
 CREATE TABLE history_songs (
-  user_id INT REFERENCES users(user_id),
-  song_name VARCHAR(512)
+  user_id   INT          NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+  song_name VARCHAR(512) NOT NULL
 );
 
 CREATE TABLE following_users (
-  user_id   INT REFERENCES users(user_id),
-  following_user_id INT REFERENCES users(user_id)
+  user_id           INT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+  following_user_id INT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE user_genre_interests (
-  user_id   INT REFERENCES users(user_id),
-  genre_id  INT REFERENCES genres(genre_id)
+  user_id   INT NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+  genre_id  INT NOT NULL REFERENCES genres(genre_id) ON DELETE CASCADE
 );
 
+-- TODO: will this be on Spotify??
 CREATE TABLE genres (
   genre_id    SERIAL PRIMARY KEY,
   genre_name  VARCHAR(512) NOT NULL
--- server/index.js
+++ server/index.js
@@ -3,14 +3,96 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const auth = require('./auth');
+
 const app = express();
 
 app.listen(process.env.PORT || 19786, () => console.log('Server is listening on port 19786'));
 
 app.use(bodyParser.json());
 
-// Sign up
-// Sign in
-// Edit profile
-// Send location
-// Get location
+// TODO: modularize below
+
+/**
+ * Sign in
+ * @body {
+ *   usr: String
+ *   psw: String
+ * }
+ * @return { { tok: String } }
+ */
+app.post('/auth', (req, res) => {
+  const { usr, psw } = req.body;
+});
+
+/**
+ * Sign up
+ * @body {
+ *   usr: String,
+ *   psw: String,
+ *   email: String
+ * }
+ */
+app.post('/user/new', (req, res) => {
+  const { usr, psw, email } = req.body;
+});
+/**
+ * Edit profile
+ * @requires Authorization
+ * @body {
+ *   usr: String,
+ *   psw: String,
+ *   email: String
+ * }
+ */
+app.put('/user', auth.check, (req, res) => {
+  const { uid } = req.user;
+});
+/**
+ * Read profile
+ * @requires Authorization
+ * @param { Number } user_id
+ */
+app.get('/user/:user_id', auth.check, (req, res) => {
+  const { uid } = req.user;
+  const { user_id } = req.params;
+});
+
+/**
+ * Status change
+ * @body {
+ *   status: 'PLAY' | 'STOP',
+ *   song: ?String
+ * }
+ */
+app.put('/status', auth.check, (req, res) => {
+  const { uid } = req.user;
+  const { status, song } = req.body;
+});
+
+/**
+ * Send location
+ * @requires Authorization
+ * @body {
+ *   lat: Double,
+ *   lng: Double
+ * }
+ */
+app.put('/location', auth.check, (req, res) => {
+  const { uid } = req.user;
+  const { lat, lng } = req.body;
+});
+
+/**
+ * Get people listening nearby
+ * @requires Authorization
+ * @returns {
+ *   nearby: {
+ *     song: String,
+ *     distance: Double
+ *   }
+ * }
+ */
+app.get('/nearby', auth.check, (req, res) => {
+  const { uid } = req.user;
+});
