d07ca2ce16e844a69a80edd7fa896fd1761dd014
Cameron Eldridge <cameldridge+git@gmail.com>
2017-09-16 00:11:34 -0400

    Change port to something less common

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const path = require('path');
 
 const app = express();
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log('Server is listening on port 8080');
+app.listen(process.env.PORT || 19786, () => {
+  console.log('Server is listening on port 19786');
 });
 
 app.use(bodyParser.json());
