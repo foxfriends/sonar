92008fa701fd70a8fbf90591a0863689551b28d2
Cameron Eldridge <cameldridge+git@gmail.com>
2017-09-16 19:58:52 -0400

    Add uitapgesturerecognizer probably in the wrong spot and making compile not work

--- HTNMusic/HTNMusic/DiscoverViewController.swift
+++ HTNMusic/HTNMusic/DiscoverViewController.swift
@@ -16,24 +16,26 @@ class DiscoverViewController: UIViewController {
     @IBOutlet fileprivate weak var userImageView: UIImageView!
     @IBOutlet fileprivate weak var listView: UIView!
     @IBOutlet fileprivate weak var nearbyTableView: UITableView!
-    
+
+    fileprivate let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.tapUserOnMap))
+
     lazy var locationManager: CLLocationManager = {
         return CLLocationManager()
     }()
-    
+
     fileprivate let viewModel = DiscoverViewModel()
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         nearbyTableView.delegate = self
-        
+
         bindViewModel()
-        
+
         self.locationManager.requestAlwaysAuthorization()
         self.locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         self.locationManager.startUpdatingLocation()
-        
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 }
@@ -53,7 +55,7 @@ fileprivate extension DiscoverViewController {
     }
 }
 
-// MARK: - UISegmentedControl 
+// MARK: - UISegmentedControl
 
 extension DiscoverViewController {
     @IBAction func indexChanged(sender: UISegmentedControl) {
@@ -73,7 +75,7 @@ extension DiscoverViewController {
 extension DiscoverViewController: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let locationValue = manager.location?.coordinate else { print("No location value"); return }
-        
+
         let locationCoordinates = Coordinate2D(longitude: Double(locationValue.longitude), latitude: Double(locationValue.latitude))
         getNearbyUsers(coords: locationCoordinates)
     }
@@ -89,7 +91,7 @@ extension DiscoverViewController: UITableViewDelegate {
         } else {
             user = viewModel.largeProximityUsers[indexPath.row - viewModel.smallProximityUsers.count - viewModel.mediumProximityUsers.count]
         }
-        
+
         if let userProfileViewController = UIStoryboard(name: "UserProfile", bundle: nil).instantiateInitialViewController() as? UserProfileViewController {
             userProfileViewController.inject(user: user, isSelf: false)
             self.present(userProfileViewController, animated: true, completion: nil)
@@ -101,7 +103,7 @@ extension DiscoverViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "nearbyCell") as! DiscoverTableViewCell
         let nearbyInfo: User
-        
+
         if indexPath.row < viewModel.smallProximityUsers.count {
             nearbyInfo = viewModel.smallProximityUsers[indexPath.row]
         } else if indexPath.row < viewModel.mediumProximityUsers.count + viewModel.smallProximityUsers.count {
@@ -109,19 +111,19 @@ extension DiscoverViewController: UITableViewDataSource {
         } else {
             nearbyInfo = viewModel.largeProximityUsers[indexPath.row - viewModel.smallProximityUsers.count - viewModel.mediumProximityUsers.count]
         }
-        
+
         cell.mediaTitleLabel.text = nearbyInfo.currentListening?.title
         cell.mediaArtistLabel.text = nearbyInfo.currentListening?.artist
         cell.userImageView.image = UIImage(named: "profile_ic")
-        
+
         return cell
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return viewModel.largeProximityUsers.count + viewModel.mediumProximityUsers.count + viewModel.smallProximityUsers.count
     }
 }
-    
+
 // MARK: - Helpers
 
 extension DiscoverViewController {
@@ -134,7 +136,7 @@ extension DiscoverViewController {
             }
         })
     }
-    
+
     func redrawUsersOnMap() {
         let origin = CGPoint(x: UIScreen.main.bounds.size.width*0.5,y: UIScreen.main.bounds.size.height*0.5)
         var counter = 1
@@ -143,75 +145,71 @@ extension DiscoverViewController {
             let smallCount = Double(viewModel.smallProximityUsers.count)
             let x = Int(floor(Double(origin.x) + radius*cos(Double(counter*2)*Double.pi/smallCount)))
             let y = Int(floor(Double(origin.y) + radius*sin(Double(counter*2)*Double.pi/smallCount)))
-            
+
             let circlePath = UIBezierPath(arcCenter: CGPoint(x: x,y: y), radius: CGFloat(radius), startAngle: CGFloat(0), endAngle:CGFloat(Double.pi * 2), clockwise: true)
-            
+
             let shapeLayer = CAShapeLayer()
             shapeLayer.path = circlePath.cgPath
-            
+
             //change the fill color
             shapeLayer.fillColor = UIColor.clear.cgColor
             //you can change the stroke color
             shapeLayer.strokeColor = UIColor.red.cgColor
             //you can change the line width
             shapeLayer.lineWidth = 3.0
-            
+
             view.layer.addSublayer(shapeLayer)
-            
+
             counter += 1
             viewModel.userMapCoordinates["\(x) \(y)"] = user
         }
-        
+
         for user in viewModel.mediumProximityUsers {
             let radius = 45.0
             let mediumCount = Double(viewModel.mediumProximityUsers.count)
             let x = floor(Double(origin.x) + radius*cos(Double(Double(counter)*2.0+Double.pi/2.0)*Double.pi/mediumCount))
             let y = floor(Double(origin.y) + radius*sin(Double(Double(counter)*2.0+Double.pi/2.0)*Double.pi/mediumCount))
-            
+
             let circlePath = UIBezierPath(arcCenter: CGPoint(x: x,y: y), radius: CGFloat(radius), startAngle: CGFloat(0), endAngle:CGFloat(Double.pi * 2), clockwise: true)
-            
+
             let shapeLayer = CAShapeLayer()
             shapeLayer.path = circlePath.cgPath
-            
+
             //change the fill color
             shapeLayer.fillColor = UIColor.purple.cgColor
             //you can change the stroke color
             shapeLayer.strokeColor = UIColor.purple.cgColor
             //you can change the line width
             shapeLayer.lineWidth = 3.0
-            
+
             view.layer.addSublayer(shapeLayer)
-            
+
             counter += 1
             viewModel.userMapCoordinates["\(x) \(y)"] = user
         }
-        
+
         for user in viewModel.largeProximityUsers {
             let radius = 65.0
             let largeCount = Double(viewModel.largeProximityUsers.count)
             let x = floor(Double(origin.x) + radius*cos(Double(Double(counter)*2.0)*Double.pi/largeCount))
             let y = floor(Double(origin.y) + radius*sin(Double(Double(counter)*2.0)*Double.pi/largeCount))
-            
+
             let circlePath = UIBezierPath(arcCenter: CGPoint(x: x,y: y), radius: CGFloat(radius), startAngle: CGFloat(0), endAngle:CGFloat(Double.pi * 2), clockwise: true)
-            
+
             let shapeLayer = CAShapeLayer()
             shapeLayer.path = circlePath.cgPath
-            
+
             //change the fill color
             shapeLayer.fillColor = UIColor.purple.cgColor
             //you can change the stroke color
             shapeLayer.strokeColor = UIColor.purple.cgColor
             //you can change the line width
             shapeLayer.lineWidth = 3.0
-            
+
             view.layer.addSublayer(shapeLayer)
-            
+
             counter += 1
             viewModel.userMapCoordinates["\(x) \(y)"] = user
         }
     }
 }
-
-
-
-
