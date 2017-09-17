a5fc5968431d8f00a9affa151697ad9210a574dd
Andre Ha <andre.oikawa@hotmail.com>
2017-09-16 21:29:16 -0400

    spotify for songList

--- server/like.js
+++ server/like.js
@@ -35,8 +35,9 @@ app.put('/:song_id', auth.check, headers, async (req, res) => {
   const { song_id } = req.params;
   const { from_user } = req.body;
   try {
-    await db.likeSong(uid, song_id, from_user);
-    res.send(result.success());
+    const songList = await db.likeSong(uid, song_id, from_user);
+    const spotlift = await spotify.lookupSongs(songList.map(_ => _.song_name));
+    res.send(result.success(spotlift.map(_ => ({ title: _.name, album: _.album.name, artist: _.artists.map(_ => _.name).join(', '), id: _.id }))));
   } catch(error) {
     res.send(result.failure(error.message));
   }
